802b7183e963bdc69db78db28ab3e6615eb274a9
azyzkabada <azyz.kabada@gmail.com>
2025-01-22 11:14:27 +0100

    Re ----

--- app/(withHeader)/(profile)/_components/Profile-form.tsx
+++ app/(withHeader)/(profile)/_components/Profile-form.tsx
@@ -6,54 +6,67 @@ import { profile } from "@/src/actions/profile"
 import { FormInput } from "@/src/components/form-input"
 import { Avatar, AvatarFallback, AvatarImage } from "@/src/components/ui/avatar"
 import { Button } from "@/src/components/ui/button"
-import { Form } from "@/src/components/ui/form"
+import { Calendar } from "@/src/components/ui/calendar"
+import { Form, FormItem, FormLabel } from "@/src/components/ui/form"
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/src/components/ui/popover"
+import ToggleTexte from "@/src/components/utils/hiddenTexte"
+import { cn } from "@/src/lib/cn-utils"
 import { zodResolver } from "@hookform/resolvers/zod"
-import { BadgeCheck, UserRound, XCircle } from "lucide-react"
+import { format } from "date-fns"
+import { BadgeCheck, CalendarIcon, UserRound, XCircle } from "lucide-react"
 import { useForm } from "react-hook-form"
 import { toast } from "sonner"
 import { z } from "zod"
 
-// Schéma de validation
+import type { ExtendedUser } from "@/types/next-auth"
+
+// Schéma de validation pour le profil
 const profileSchema = z.object({
   name: z.string().min(1, "Le nom est requis"),
   email: z.string().email("Adresse e-mail invalide"),
-  phone: z.string().min(10, "Numéro de téléphone invalide").optional(),
-  dateOfBirth: z
-    .string()
-    .refine((value) => !isNaN(Date.parse(value)), "Date de naissance invalide")
-    .optional(),
+  password: z.string().optional(),
+  newPassword: z.string().optional(),
+  isTwoFactorEnabled: z.boolean().optional(),
+  dateOfBirth: z.date({
+    required_error: "La date de naissance est requise.",
+  }),
 })
 
-// Types dérivés
+// Type dérivé du schéma de validation
 type ProfileSchema = z.infer<typeof profileSchema>
 
-// Props pour le composant
+// Interface pour un utilisateur
 interface User {
   name: string
   email: string
-  phone?: string
-  dateOfBirth?: string
   image: string | null
   id: string
+  role: "User" | "Admin"
+  isTwoFactorEnabled: boolean
+  isOAuth: boolean
   emailVerified: boolean
 }
 
+// Props pour le composant de dialogue des paramètres
 interface SettingsDialogProps {
-  user: User
+  user: ExtendedUser
 }
 
 export default function SettingsDialog({ user }: SettingsDialogProps) {
   const [isPending, startTransition] = useTransition()
   const router = useRouter()
-
   const form = useForm<ProfileSchema>({
     resolver: zodResolver(profileSchema),
     mode: "onChange",
     defaultValues: {
-      name: user.name,
-      email: user.email,
-      phone: user.phone || "",
-      dateOfBirth: user.dateOfBirth || "",
+      name: user?.name || "John Doe",
+      email: user?.email || "john.doe@example.com",
+      phone: user?.phone,
+      dateOfBirth: undefined,
     },
   })
 
@@ -71,73 +84,112 @@ export default function SettingsDialog({ user }: SettingsDialogProps) {
   })
 
   return (
-    <main className="flex-1 mt-4">
-      <div className="mx-auto md:container">
-        <div className="space-y-8 bg-white ">
+    <main className="flex-1">
+      <div className="w-full py-6 md:container">
+        <div className="max-w-4xl mx-auto space-y-8">
           <div className="text-center">
             <Avatar className="w-24 h-24 mx-auto">
-              <AvatarImage src={user.image || ""} alt={user.name} />
+              <AvatarImage
+                src={user.image as string}
+                alt={user.name as string}
+              />
               <AvatarFallback>
-                <UserRound className="w-12 h-12 text-gray-500" />
+                <UserRound className="w-12 h-12" />
               </AvatarFallback>
             </Avatar>
             <h1 className="mt-4 text-2xl font-bold">{user.name}</h1>
-            <p className="text-sm text-gray-600">{user.email}</p>
-            <div className="mt-2">
+            <div className="flex items-center justify-center gap-4 mt-2 text-xs">
+              ID Client :
+              <ToggleTexte className="text-xs" texte={user.id} />
+            </div>
+            <div className="flex items-center justify-center gap-2 mt-2 text-sm">
+              <span>{user.email}</span>
               {user.emailVerified ? (
-                <span className="px-2 py-1 text-sm text-green-600 bg-green-100 ">
-                  <BadgeCheck className="inline w-4 h-4 mr-1" />
+                <span className="flex items-center px-2 py-1 text-green-700 bg-green-200">
+                  <BadgeCheck className="w-4 h-4 mr-1" />
                   Vérifié
                 </span>
               ) : (
-                <button
-                  className="px-2 py-1 text-sm text-red-600 bg-red-100 hover:bg-red-200"
+                <div
+                  className="flex items-center px-2 py-1 text-red-700 bg-red-200 cursor-pointer hover:bg-red-300 hover:text-red-600"
                   onClick={() =>
                     router.push(
-                      `/resend-email?email=${encodeURIComponent(user.email)}`
+                      `/resend-email?email=${encodeURIComponent(
+                        user.email as string
+                      )}`
                     )
                   }
                 >
-                  <XCircle className="inline w-4 h-4 mr-1" />
+                  <XCircle className="w-4 h-4 mr-1" />
                   Non vérifié
-                </button>
+                </div>
               )}
             </div>
           </div>
 
           <Form {...form}>
-            <form onSubmit={handleSubmit} className="space-y-6">
-              <div className="grid grid-cols-1 gap-6 sm:grid-cols-2">
+            <form onSubmit={handleSubmit} className="w-full space-y-6">
+              <div className="grid w-full gap-6 sm:grid-cols-2">
                 <FormInput
+                  className="w-full"
+                  control={form.control}
                   name="name"
                   label="Nom et prénom"
                   type="text"
                   placeholder="ex : John Doe"
+                  isPending={isPending}
+                />
+                {/* DatePicker */}
+                <FormItem>
+                  <FormLabel>Date de naissance</FormLabel>
+                  <Popover>
+                    <PopoverTrigger asChild>
+                      <Button
+                        variant="outline"
+                        className={cn(
+                          "w-full px-4 py-2 text-left",
+                          !form.watch("dateOfBirth") && "text-muted-foreground"
+                        )}
+                      >
+                        {form.watch("dateOfBirth")
+                          ? format(form.watch("dateOfBirth"), "PPP")
+                          : "Sélectionnez une date"}
+                        <CalendarIcon className="w-4 h-4 ml-auto opacity-50" />
+                      </Button>
+                    </PopoverTrigger>
+                    <PopoverContent className="w-auto p-0" align="start">
+                      <Calendar
+                        mode="single"
+                        selected={form.watch("dateOfBirth")}
+                        onSelect={(date) => form.setValue("dateOfBirth", date)}
+                        disabled={(date) =>
+                          date > new Date() || date < new Date("1900-01-01")
+                        }
+                      />
+                    </PopoverContent>
+                  </Popover>
+                </FormItem>
+                <FormInput
                   control={form.control}
-                  className="w-full"
+                  disabled
+                  name="email"
+                  label="Adresse e-mail"
+                  type="email"
+                  placeholder="ex : johndoe@example.com"
                   isPending={isPending}
                 />
                 <FormInput
-                  name="phone"
-                  label="Numéro de téléphone"
-                  type="tel"
-                  placeholder="ex : +33123456789"
                   control={form.control}
-                  className="w-full"
+                  name="phone"
+                  label="PhoneNumber"
+                  type="phone"
+                  placeholder="ex : +216 0980980980"
                   isPending={isPending}
                 />
               </div>
-              <FormInput
-                name="dateOfBirth"
-                label="Date de naissance"
-                type="date"
-                placeholder="ex : 1990-01-01"
-                control={form.control}
-                className="w-full"
-                isPending={isPending}
-              />
-              <Button type="submit" className="w-full" disabled={isPending}>
-                Mettre à jour
+
+              <Button type="submit" disabled={isPending} className="w-full">
+                Mettre à jour le profil
               </Button>
             </form>
           </Form>
--- app/(withHeader)/_homepage/HeroSection.tsx
+++ app/(withHeader)/_homepage/HeroSection.tsx
@@ -4,39 +4,36 @@ import Image from "next/image"
 import Link from "next/link"
 import { buttonVariants } from "@/src/components/ui/button"
 
-import { siteConfig } from "@/config/site"
-
 export default function HeroSection() {
   return (
-    <section className="container px-4 py-10 ">
+    <section className="container px-4 py-10">
       <div className="flex flex-col-reverse items-center gap-8 md:flex-row md:justify-between">
         <div className="flex flex-col items-center text-center md:w-1/2 md:items-start md:text-left">
           <h1 className="text-xl font-extrabold leading-tight tracking-tighter font-special md:text-3xl lg:text-4xl">
-            Beautifully designed components
+            Offrez la beauté des fleurs
             <span className="block mt-2 text-primary">
-              built with Radix UI and Tailwind CSS.
+              pour chaque occasion spéciale.
             </span>
           </h1>
           <p className="mt-4 max-w-[600px] text-lg text-muted-foreground">
-            Accessible and customizable components that you can copy and paste
-            into your apps. Free. Open Source. And Next.js 13 Ready.
+            Découvrez notre sélection de bouquets magnifiquement conçus et de
+            cadeaux personnalisés. Livraison rapide, soin garanti, et service
+            impeccable.
           </p>
           <div className="flex flex-col gap-4 mt-8 sm:flex-row">
             <Link
-              href={siteConfig.links.docs}
-              target="_blank"
-              rel="noreferrer"
+              href={"/"}
+              target="_self"
               className={buttonVariants({ size: "lg" })}
             >
-              Documentation
+              Explorez notre boutique
             </Link>
             <Link
-              target="_blank"
-              rel="noreferrer"
-              href={siteConfig.links.github}
+              target="_self"
+              href="/about"
               className={buttonVariants({ variant: "outline", size: "lg" })}
             >
-              GitHub
+              En savoir plus
             </Link>
           </div>
         </div>
